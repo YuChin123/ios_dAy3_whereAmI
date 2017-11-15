29ffb9f649c4051441d362a67b320a613293dc32
Cyberjaya 4 iTrain <cyberjaya4itrain@Cyberjayas-iMac-6.local>
2017-11-15 10:53:43 +0800

    first commit

--- whereAmI/ViewController.swift
+++ whereAmI/ViewController.swift
@@ -8,12 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import CoreLocation
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var lbl_latitude: UILabel!
+    @IBOutlet var lbl_longtitude: UILabel!
+    @IBOutlet var lbl_address: UILabel!
+    
+    var myLocationManager: CLLocationManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+ 
+        //Initialise the Manager
+        self.myLocationManager = CLLocationManager()
+        
+        //to able to use the implemented delegate method
+        self.myLocationManager.delegate = self
+        
+        //Asking Permission
+        self.myLocationManager.requestWhenInUseAuthorization()
+        
+        //Start to grab user location
+        self.myLocationManager.startUpdatingLocation()
     }
+    
+    //whenever detect location, this will be triggered
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //Getting the lastest location from the set of results
+        if let currentLocation = locations.last {
+
+            //Display the result into the label
+            self.lbl_latitude.text = "latitude is \(currentLocation.coordinate.latitude)"
+            
+        self.lbl_longtitude.text = "longtitude is \(currentLocation.coordinate.longitude)"
+        }
+       
+        
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
