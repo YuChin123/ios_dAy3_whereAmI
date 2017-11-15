2ebd2017b056628adbeb229287f90b6598258ae6
Cyberjaya 4 iTrain <cyberjaya4itrain@cyberjayas-imac-6.local>
2017-11-15 09:57:24 +0800

    Initial Commit

---
+++ whereAmI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whereAmI
+//
+//  Created by Cyberjaya 4 iTrain on 15/11/2017.
+//  Copyright © 2017 ada_yc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
